048f95b69d97fd810798637580d84e99850b4e0e
rust-benchmark1 <rust-benchmark@proton.me>
2025-07-10 12:24:58 -0300

    CWE 90 Update comments

--- src/core/server.rs
+++ src/core/server.rs
@@ -331,8 +331,8 @@ async fn receive_sql_data() -> anyhow::Result<String> {
 async fn receive_ldap_data() -> anyhow::Result<String> {
     let socket = TokioUdpSocket::bind("127.0.0.1:8085").await?;
     let mut buffer = [0; 1024];
-    let (bytes_read, _src_addr) = socket.recv_from(&mut buffer).await?;
     //SOURCE
+    let (bytes_read, _src_addr) = socket.recv_from(&mut buffer).await?;
     let received_data = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
     Ok(received_data)
 }
